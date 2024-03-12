5d4839819a7a56062ad13a607b1c8b076816ea4b
Alexandr1997ag <87136289+Alexandr1997ag@users.noreply.github.com>
2024-03-12 08:51:16 +0300

    Update args_test.py

--- args_test.py
+++ args_test.py
@@ -15,11 +15,11 @@ def main():
     model.conf = 0.20
 
 
-    img = Image.open(args.image_path)#.resize((640, 640))  
+    img = Image.open(args.image_path) 
     start_time = time.time()
 
     # results = model(img, size=640) 
-    results = model(img, size=640) 
+    results = model(img) 
     end_time = time.time()
     elapsed_time = end_time - start_time
 
